3f88012df8c134babd35a2068d6207764b9656d6
Leandro Casaretto <leandro.casaretto@apollo.io>
2026-02-23 12:37:47 -0300

    update

--- init.lua
+++ init.lua
@@ -148,12 +148,29 @@ require('lazy').setup({
     },
   },
 
-  { -- cursor cli agent
-    'xTacobaco/cursor-agent.nvim',
-    config = function()
-      vim.keymap.set('n', '<leader>aa', ':CursorAgent<CR>', { desc = 'Cursor Agent: Toggle terminal' })
-      vim.keymap.set('n', '<leader>ab', ':CursorAgentBuffer<CR>', { desc = 'Cursor Agent: Send buffer' })
-    end,
+  { --claude code
+    'coder/claudecode.nvim',
+    dependencies = { 'folke/snacks.nvim' },
+    config = true,
+    keys = {
+      { '<leader>a', nil, desc = 'AI/Claude Code' },
+      { '<leader>ac', '<cmd>ClaudeCode<cr>', desc = 'Toggle Claude' },
+      { '<leader>af', '<cmd>ClaudeCodeFocus<cr>', desc = 'Focus Claude' },
+      { '<leader>ar', '<cmd>ClaudeCode --resume<cr>', desc = 'Resume Claude' },
+      { '<leader>aC', '<cmd>ClaudeCode --continue<cr>', desc = 'Continue Claude' },
+      { '<leader>am', '<cmd>ClaudeCodeSelectModel<cr>', desc = 'Select Claude model' },
+      { '<leader>ab', '<cmd>ClaudeCodeAdd %<cr>', desc = 'Add current buffer' },
+      { '<leader>as', '<cmd>ClaudeCodeSend<cr>', mode = 'v', desc = 'Send to Claude' },
+      {
+        '<leader>as',
+        '<cmd>ClaudeCodeTreeAdd<cr>',
+        desc = 'Add file',
+        ft = { 'NvimTree', 'neo-tree', 'oil', 'minifiles', 'netrw' },
+      },
+      -- Diff management
+      { '<leader>aa', '<cmd>ClaudeCodeDiffAccept<cr>', desc = 'Accept diff' },
+      { '<leader>ad', '<cmd>ClaudeCodeDiffDeny<cr>', desc = 'Deny diff' },
+    },
   },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
